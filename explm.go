67584e06b56eff434973ae9b2620cb2cf41c0dcb
ivan <ivanzhangsolutions@gmail.com>
2016-09-02 09:53:25 -0400

    using go query to parse now, prints title

--- explm.go
+++ explm.go
@@ -1,51 +1,47 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "golang.org/x/net/html"
-    "io"
-    "log"
-    "net/http"
-    "os"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Fprintf(os.Stderr, "Usage: %s {$meme_name}\n", os.Args[0])
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s {$meme_name}\n", os.Args[0])
+		os.Exit(1)
+	}
 
-    get_meme(os.Args[1])
+	get_meme(os.Args[1])
 }
 
 func get_meme(meme_name string) {
-    var url bytes.Buffer
-    url.WriteString("http://knowyourmeme.com/memes/")
-    url.WriteString(meme_name)
+	var url bytes.Buffer
+	url.WriteString("http://knowyourmeme.com/memes/")
+	url.WriteString(meme_name)
 
-    rs, err := http.Get(url.String())
-    if err != nil {
-        log.Fatal(err)
-    } else {
-        defer rs.Body.Close()
-        if err != nil {
-            log.Fatal(err)
-        } else {
-            parse_html(rs.Body)
-        }
-    }
+	doc, err := goquery.NewDocument(url.String())
+	if err != nil {
+		log.Fatalln("Error, unable to parse webpage: ", err)
+	}
+
+	log.Println(doc.Find("h1").First().Find("a").Text())
 }
 
 // http://schier.co/blog/2015/04/26/a-simple-web-scraper-in-go.html
 
-func parse_html (body io.Reader) {
-    ttz := html.NewTokenizer(body)
-    for {
-        tt := ttz.Next()
-        if tt == html.ErrorToken{
-            break
-        }
-        // consume the token
-    }
+func parse_html(body io.Reader) {
+	ttz := html.NewTokenizer(body)
+	for {
+		tt := ttz.Next()
+		if tt == html.ErrorToken {
+			break
+		}
+		// consume the token
+	}
 }
